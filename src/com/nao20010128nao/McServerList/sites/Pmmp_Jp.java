529058925c135c70ad83bb8dcbf0ee93463dc02e
nao20010128nao <nao20010128@gmail.com>
2016-03-11 20:08:11 +0900

    追加だけ

---
+++ src/com/nao20010128nao/McServerList/sites/Pmmp_Jp.java
@@ -0,0 +1,32 @@
+package com.nao20010128nao.McServerList.sites;
+
+import java.net.URL;
+import java.util.List;
+
+import com.nao20010128nao.McServerList.Server;
+
+public class Pmmp_Jp implements ServerListSite {
+
+	public Pmmp_Jp() {
+		// TODO 自動生成されたコンストラクター・スタブ
+	}
+
+	@Override
+	public boolean matches(URL url) {
+		// TODO 自動生成されたメソッド・スタブ
+		return false;
+	}
+
+	@Override
+	public boolean hasMultipleServers(URL url) {
+		// TODO 自動生成されたメソッド・スタブ
+		return false;
+	}
+
+	@Override
+	public List<Server> getServers(URL url) {
+		// TODO 自動生成されたメソッド・スタブ
+		return null;
+	}
+
+}
